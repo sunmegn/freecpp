58cbbe6cb913802e6b1e2514d7661b3b00414d2e
smake <Mr_smake@hotmail.com>
2018-08-04 12:32:50 +0800

    add

---
+++ freecpp/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main() {
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl; cout << "You won't regret it!" << endl;
+	/*cin.get();*/
+	return 0;
+}
+// #include <iostream>
+
+// int s;
+
+// int main()
+// {
+//     std::cout<<"hello world!"<<std::endl;
+//     std::cout<<"hello"<<std::endl;
+//     std::cin>>s;
+//     std::cout<<"s ="<<s<<std::endl;
+//     return 0;
+// }
